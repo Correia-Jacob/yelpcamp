c14edb4c11658f2e1b29b0f7c8b2e5d992712d77
Jacob Correia <89880589+Correia-Jacob@users.noreply.github.com>
2022-08-11 08:07:15 -0400

    implemented basic CRUD infrastructure for campgrounds

--- app.js
+++ app.js
@@ -1,35 +1,69 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const Campground = require('./models/campground');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+const methodOverride = require("method-override");
+const app = express();
 
-mongoose.connect('mongodb://localhost:27017/yelpcamp', {
-    newUrlParser: true,
-    useCreateIndex: true,
+mongoose.connect("mongodb://localhost:27017/yelpcamp", {
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'error:'));
-db.once('open', () => {
-    console.log('connected to db');
+db.on("error", console.error.bind(console, "error:"));
+db.once("open", () => {
+    console.log("connected to db");
 });
 
-const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+    res.render("home");
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'backyard', description: 'camping in the backyard'});
-    await camp.save();
-    res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", {campgrounds});
+});
+
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get("/campgrounds/:id", async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", {campground});
+});
+
+app.get("/campgrounds/:id/edit", async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", {campground});
+});
+
+app.put("/campgrounds/:id", async(req, res) => {
+ const {id} = req.params;
+ const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+});
+
+app.delete("/campgrounds/:id", async(req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
 });
 
 app.listen(3000, () => {
-    console.log('Opening app.js')
+    console.log("Opening app.js")
 });
